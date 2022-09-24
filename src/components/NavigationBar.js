014d8b69e5288d7f75770f1e49cbb141ee98b396
john <john.flouis1@gmail.com>
2022-09-24 15:59:17 +0200

    Update navigation bar using React

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./global.css"
+import logo from "../assets/logo/playera-light.png";
+
+export default function NavigationBar() {
+    return ( 
+        <header className="header">
+            <a> <img src={logo} alt="Logo" className="header__img" /> </a>
+            <nav className="header__nav">
+                <ul className="header__list">
+                    <li> <a className="header__ul__a">Home</a> </li>
+                    <li> <a className="header__ul__a header__dropdown">Games</a> </li>
+                    <li> <a className="header__ul__a header__call-to-action">Contact</a> </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
